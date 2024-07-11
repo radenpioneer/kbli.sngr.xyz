f6096d4690aa8e38a171f990146c702ae0e38664
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-07-11 23:40:08 +0000

    RESET

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,4 @@
 import { defineConfig } from 'astro/config'
-import { loadEnv } from 'vite'
-import htmx from 'astro-htmx'
-import alpinejs from '@astrojs/alpinejs'
-import react from '@astrojs/react'
-import markdoc from '@astrojs/markdoc'
-import keystatic from '@keystatic/astro'
-import cloudflare from '@astrojs/cloudflare'
-
-const { KEYSTATIC } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    htmx(),
-    alpinejs(),
-    react(),
-    markdoc(),
-    ...(KEYSTATIC ? [keystatic()] : [])
-  ],
-  output: 'hybrid',
-  adapter: cloudflare()
-})
+export default defineConfig({})
